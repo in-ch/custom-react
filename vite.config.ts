8e10d400d1184e4d3b9ca9aa5549e763d8d8e5da
in-ch <sxin2949@naver.com>
2024-09-17 23:11:56 +0900

    set up Vite development environment

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  optimizeDeps: {
-    include: [],
+  server: {
+    port: 3000,
   },
   plugins: [
     react({
